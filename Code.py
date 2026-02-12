a3e5aee7874306fed2e4b6b10ee53c04746a5b99
vivvanjain9-afk <vivvanjain9@gmail.com>
2026-02-12 15:52:02 +0000

    Update Code.py

--- Code.py
+++ Code.py
@@ -1,32 +1,28 @@
-computer = range(1,3)
-if computer ==1:
-    choice = rock
-elif computer ==2:
-    choice = paper
+computer = ""
+x = 0
+user_choice = input("Enter your choice, rock, paper, or scissors:")
+if user_choice == "rock" or user_choice == "Rock" or user_choice == "ROCK":
+    computer = "paper"
+    print("The computer chose:" + computer)
+    print("You chose:" + user_choice)
+    print("The computer [always] wins!")
+elif user_choice == "paper" or user_choice == "Paper" or user_choice == "PAPER":
+    computer = "Scissors"
+    print("The computer chose:" + computer)
+    print("You chose:" + user_choice)
+    print("The computer [always] wins!")
+elif user_choice == "scissors" or user_choice == "Scissors" or user_choice == "SCISSORS":
+    computer = "rock"
+    print("The computer chose:" + computer)
+    print("You chose:" + user_choice)
+    print("The computer [always] wins!")
+elif user_choice == "win" or user_choice == "Win" or user_choice == "WIN":
+    print("Finally, you figured out the forbidden word, you won")
 else:
-    choice = scissors
-user_choice =input("Enter a choice, rock, paper, or scissors:")
-if user_choice == "rock" or "Rock":
-    if choice == "rock":
-        print("It's a tie")
-    elif choice == "paper":
-        print("You lost")
-    else:
-        print("You won")
-elif user_choice == "paper" or "Paper":
-    if choice == "rock":
-        print("You won")
-    elif choice == "paper":
-        print("It's a tie")
-    else:
-        print("You lost") 
-elif user_choice == "scissors" or "Scissors":
-    if choice == "rock":
-        print("You lost")
-    elif choice == "paper":
-        print("You won")
-    else:
-        print("It's a tie")
-else:
-    print("Invalid choice")
-#this is my project
+    print("Invalid choice try again")
+if x == 10:
+    print("So you see, you can never win, unless you use the forbidden method)")
+if x == 20:
+    print("The forbidden word is win")
+x = x + 1
+
